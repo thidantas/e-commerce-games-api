ec9db48cb00aea529ddfa8fbf39a197f8a713c8d
thidantas <eng.thiagohpdantas@gmail.com>
2025-03-28 14:50:45 -0300

    chore(api): fetching data from GOG for validation

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -2,6 +2,17 @@
  * game service
  */
 
-import { factories } from '@strapi/strapi';
+import axios from "axios";
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreService('api::game.game');
+export default factories.createCoreService("api::game.game", () => ({
+  async populate(params) {
+    const gogApiUrl = `https://catalog.gog.com/v1/catalog?limit=48&order=desc%3atrending`;
+
+    const {
+      data: { products },
+    } = await axios.get(gogApiUrl);
+
+    console.log(products[0]);
+  },
+}));
