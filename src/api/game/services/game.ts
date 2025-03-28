b71f0b6afd9001e5f6021f48adf19695ddaa2350
thidantas <eng.thiagohpdantas@gmail.com>
2025-03-28 14:59:01 -0300

    feat(scraper): implement data extraction using JSDOM

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -3,7 +3,37 @@
  */
 
 import axios from "axios";
+import { JSDOM } from "jsdom";
 import { factories } from "@strapi/strapi";
+
+async function getGameInfo(slug) {
+  const gogSlug = slug.replaceAll("-", "_").toLowerCase();
+
+  const body = await axios.get(`https://www.gog.com/game/${gogSlug}`);
+
+  const dom = new JSDOM(body.data);
+
+  const raw_description = dom.window.document.querySelector(".description");
+
+  const description = raw_description.innerHTML;
+
+  const short_description = raw_description.textContent.slice(0, 160);
+
+  const ratingElement = dom.window.document.querySelector(
+    ".age-restrictions__icon use"
+  );
+
+  return {
+    description,
+    short_description,
+    rating: ratingElement
+      ? ratingElement
+          .getAttribute("xlink:href")
+          .replace(/_/, "")
+          .replace("#", "")
+      : "BR0",
+  };
+}
 
 export default factories.createCoreService("api::game.game", () => ({
   async populate(params) {
@@ -13,6 +43,6 @@ export default factories.createCoreService("api::game.game", () => ({
       data: { products },
     } = await axios.get(gogApiUrl);
 
-    console.log(products[0]);
+    console.log(getGameInfo(products[0].slug));
   },
 }));
